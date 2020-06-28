c2d70331b7284ef247b8aac444016556c16c7ac1
oszikr <oszikr@gmail.com>
2020-06-29 01:07:41 +0200

    Service file for Qt app.

---
+++ service.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from colorama import Fore, Back, Style
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model_from_json
+import json
+import util
+
+"""
+Predicting 
+"""
+
+def predictOne(X):
+    y_pred = predict([X])
+    return y_pred[0]
+
+def predict(X):
+    for _ in range(len(X)):
+        X[_] = labelencoder.transform(X[_])
+
+    X = onehotencoder.transform(X).toarray()
+
+    X = util.remove3rdcols(X)
+
+    y_pred = nnet.predict(X)
+    return y_pred
+
+if __name__ == "__main__":
+    nnet = util.getnnet()
+    labelencoder = util.getlabelencoder()
+    onehotencoder = util.getonehotencoder()
+    while True:
+        print("Enter input")
+        inputStr = input()
+        if inputStr in ("quit", "q"):
+            exit(0)
+        #inputJson = "[0,0,0,0,0,0,0,1,1,0,1,2,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,2,1,2,2,0,0,0,0,0,0,0,0,0,2,2,1,2,0,0,0,0,0,0,0,0,1,2,1,2,0,0,0,0,0,0,0,1,1,2,1,2,1,0,0,0,0,0,1,0,2,0,2,1,1,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,0,0,0,2,1,1,1,0,0,0,0,0,0,0,1,0,2,0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,0,0,0,0,0,0,0,2,1,0,0,0,0,0,0,0,0]"
+        param = json.loads(inputStr)
+        Y = predictOne(param)
+        print(Y)
